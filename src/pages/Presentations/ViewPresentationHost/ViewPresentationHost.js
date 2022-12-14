ea50386335ea1fea67c98b77fcf19830e56b36e9
Đoàn Thu Ngân <55989458+thungan1909@users.noreply.github.com>
2022-12-14 21:38:13 +0700

    view present

--- src/pages/Presentations/ViewPresentationHost/ViewPresentationHost.js
+++ src/pages/Presentations/ViewPresentationHost/ViewPresentationHost.js
@@ -18,6 +18,7 @@ export default function ViewPresentationHost ()
             <div className="slideshow__content">
                 <div className="slideshow__header">
                     <p> Go to www.menti.com and use the code  {slidecode} </p>
+                    
                 </div>
                 <div className="slideshow__wrapper">
                     <div className="slideshow__title">
@@ -25,7 +26,16 @@ export default function ViewPresentationHost ()
                     </div>
 
                     <div className="slideshow__graph">
-                            
+                        <svg class="chart" width="420" height="150" aria-labelledby="title desc" role="img">
+                            <g class="bar-group" fill="#196cff" transform="">
+                                <rect class="bar-item" x="121" y="0" height="150" width="100" fill="#196cff" stroke="#196cff" stroke-width="3" stroke-opacity="0" opacity="1"></rect>
+                            </g>   
+                            <g class="bar-group" fill="#196cff" transform="">
+                                <rect class="bar-item" x="121" y="0" height="150" width="100" fill="#196cff" stroke="#196cff" stroke-width="3" stroke-opacity="0" opacity="1"></rect>                            </g>  
+                            <g class="bar-group" fill="#196cff" transform="">
+                                <rect class="bar-item" x="121" y="0" height="150" width="100" fill="#196cff" stroke="#196cff" stroke-width="3" stroke-opacity="0" opacity="1"></rect>
+                            </g>                            
+                        </svg>
                     </div>
                 </div>
                
