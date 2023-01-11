df79545e485bd9a30e06cdd7e680fa142ed832a0
Đoàn Thu Ngân <55989458+thungan1909@users.noreply.github.com>
2023-01-12 01:27:26 +0700

    presentation public

--- src/pages/Presentations/PresentationList/PresentationList.js
+++ src/pages/Presentations/PresentationList/PresentationList.js
@@ -13,48 +13,10 @@ import DeletePresentationAlertDialog from '../../Group/GroupPresentation/DeleteP
 import axios from 'axios'
 import { useAuth } from '../../../context/AuthProvider'
 
-const presents = [
-    
-    {
-        id: 1,
-        name: "My presentation",
-        owner: "dtn2001",
-        modifiedDay: "12/12/2022",
-        createdDay: "09/12/2022",
-    },
-    {
-        id: 2,
-        name: "WebNC presentation",
-        owner: "thuytrinh01",
-        modifiedDay: "02/12/2022",
-        createdDay: "04/11/2022",
-    },
-    {
-        id: 3,
-        name: "KCPM's presentation",
-        owner: "ducminh02",
-        modifiedDay: "14/12/2022",
-        createdDay: "01/12/2022",
-    },
-    {
-        id: 4,
-        name: "Mobile",
-        owner: "tuyetnhu03",
-        modifiedDay: "2/12/2022",
-        createdDay: "10/11/2022",
-    },
-    {
-        id: 5,
-        name: "GB07",
-        owner: "anhminh2001",
-        modifiedDay: "03/12/2022",
-        createdDay: "22/11/2022",
-    },
-]
 function PresentationList ()
 {
     const { token } = useAuth();
-
+    const [newPresent, setNewPresent] = useState();
     const [presentList, setPresentList] = useState([]);
     const [openDeleteConfirm, setOpenDeleteConfirm] = useState(false);
     const [deletePresentationState, setDeletePresentationState]  = useState();
@@ -71,37 +33,31 @@ function PresentationList ()
             DeletePresentation(token, presentDlt, setDeletePresentationState);
     }, [confirmDelete]);
 
-
-    // set list data
-    // useEffect(() => {
-    //     ListPresentation(setPresentList);
-    //     setConfirmDelete(false);
-    // }, [newPresent,deletePresentationState]);
-    //Load and show presentation
-    // useEffect(() => {
-    //     const token = localStorage.getItem("token");
-    //     async function loadGroups () {
-    //         const res = await axios.get(`${API_URL}presentation`, {
-    //             headers: {
-    //                 Authorization: `Bearer ${token}`
-    //             }
-    //         });
-    //     setPresentList(res.data.Presentations);
-    //     }
-    //     loadGroups();
-    // }, []);
-
     useEffect(() => {
+        if (token) {
+            ListPresentation(token,setPresentList);
+          //  console.log(presentList);
+        }
+    }, [token]);
+    // useEffect(() => {
+    //     if (token) {
+    //         ListPresentation(token,setPresentList);
+    //         console.log(presentList);
+    //         setConfirmDelete(false)
+    //     }
+    // }, [token, newPresent, deletePresentationState]);
 
-        ListPresentation(setPresentList);
-        // GetPresentationList(setPresentList);
 
-        //create a list presentations by Iterate through elements in an Array with map()
-        // var listofPresents = [];
-        // listofPresents = presents.map(present => present);
-        // //update this list presentations by useSate
-        // setPresentList(listofPresents);        
-    }, []);
+    // useEffect(() => {
+    //     ListPresentation(token,setPresentList);
+    //     // GetPresentationList(setPresentList);
+
+    //     //create a list presentations by Iterate through elements in an Array with map()
+    //     // var listofPresents = [];
+    //     // listofPresents = presents.map(present => present);
+    //     // //update this list presentations by useSate
+    //     // setPresentList(listofPresents);        
+    // }, []);
    
     return (
         
@@ -110,12 +66,12 @@ function PresentationList ()
              <PresentationButtonBar list={presentList} setPresentList={setPresentList}/> 
             <PresentationListBar />
            
-            {presentList.length === 0 ? (
+            {/* {presentList.length === 0 ? (
                 <div className='no-presentation'>
                     <h3>No presentations!</h3>
                 </div>) 
                 : (<PresentationElement list={presentList} dltFunc={deletePresent}/>)
-            }
+            } */}
       </div>
     )
 }
